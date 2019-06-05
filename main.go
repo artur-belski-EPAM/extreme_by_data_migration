3faa1de37484b6c1846cd83ac51c6304b8a8288f
Artur_Belski <artur_belski@epam.com>
2019-06-05 12:36:56 +0300

    add func

--- main.go
+++ main.go
@@ -2,44 +2,41 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
-	//   "net/http"
-	// 	"os"
-	// 	"strings"
-	// 	"golang.org/x/net/html"
-
 	"log"
+	"net/http"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
 	CrowlTopics("http://extreme.by/forum/forum28.html")
+	// CrowlTopics("http://github.com/PuerkitoBio/goquery/wiki/Tips-and-tricks")
 }
 
 func CrowlTopics(url string) {
 	log.Println("Start")
 	res, err := http.Get(url)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Get request error:", err)
 	}
-
 	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+		log.Fatalf("Status code error: %d %s", res.StatusCode, res.Status)
 	}
 	fmt.Println(res)
 	doc, err := goquery.NewDocumentFromReader(res.Body)
-	doc.Find(".topictitle").Each(func(i int, s *goquery.Selection) {
+	if err != nil {
+		log.Fatal("Parsing error:", err)
+	}
+	log.Println("Parsing done")
+	topics := doc.Find(".topictitle")
+	fmt.Printf("found topics: %d \n", topics.Size())
+	topics.Each(func(i int, s *goquery.Selection) {
 		title := s.Find("i").Text()
-		fmt.Printf("Topic %d: %s\n", i, title)
+		fmt.Printf("==================>Topic %d: %s\n", i, title)
 	})
 
-	if err != nil {
-		log.Fatal(err)
-	}
 	log.Println("Done")
 
 }
