a1479e10e2160c07be51ca2a6694409bd6188efe
Artur_Belski <artur_belski@epam.com>
2019-06-05 11:57:50 +0300

    some changes

--- main.go
+++ main.go
@@ -15,23 +15,31 @@ import (
 )
 
 func main() {
-	log.Print("Start")
-	res, err := http.Get("http://m.extreme.by/forum/forum28.html?sid=49ffd60e53d2e8b69988cf46eb48894d")
+	CrowlTopics("http://extreme.by/forum/forum28.html")
+}
+
+func CrowlTopics(url string) {
+	log.Println("Start")
+	res, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
 		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
 	}
-
+	fmt.Println(res)
 	doc, err := goquery.NewDocumentFromReader(res.Body)
-	fmt.Print(doc)
-
-	log.Print("Done")
+	doc.Find(".topictitle").Each(func(i int, s *goquery.Selection) {
+		title := s.Find("i").Text()
+		fmt.Printf("Topic %d: %s\n", i, title)
+	})
 
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Println("Done")
 
 }
